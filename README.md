c32ee63ee2b675dac3e9bcd79714526a49e7c231
Neil <neil.mcalpine@musicqubed.com>
2016-06-07 22:20:55 +0300

    updates

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-Remov file server/lib/flask/ext/setuptools/script (dev).tmpl otherwise get deploy failure
+For running locally
+1. Create a python virtual environment 
+	virtualenv env
+2. Activate the environment
+    env\scripts\activate
+3. Install packages into this environment
+    pip install -r requirements.txt
 
+When Deploying
+- Remove file server/lib/flask/ext/setuptools/script (dev).tmpl otherwise get deploy failure
 
--- main.py
+++ main.py
@@ -14,15 +14,82 @@
 
 # [START app]
 from flask import Flask
-
+from gcloud import datastore
+import sys
+import urllib2
+import json
+from oauth2client.client import GoogleCredentials
+import httplib2
+from oauth2client.contrib.appengine import AppAssertionCredentials
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
+
+    credentials = AppAssertionCredentials([])
+    client = datastore.Client(project = 'mq-cloud-prototyping-3', credentials = credentials)
+    sys.stdout.write(credentials.to_json())
+    
+    query = client.query(kind='Person')
+    res = query.fetch()
+    all = dict(res)
+    sys.stdout.write(str(all))
+    
+    return credentials.to_json()
+
+    try :        
+    	
+    	token = ''
+
+    	#ouath
+        O_AUTH_EMAIL_SCOPE = 'https://www.googleapis.com/auth/userinfo.email'
+
+        credentials = GoogleCredentials.get_application_default()
+        if credentials.create_scoped_required():
+	        credentials = credentials.create_scoped(PUBSUB_SCOPES)
+        http = httplib2.Http()
+        credentials.authorize(http)
+
+        cl = discovery.build('pubsub', 'v1', http=http)
+        return credentials.to_json()
+
+        credentials = GoogleCredentials.get_application_default()
+        credentials = credentials.create_scoped([O_AUTH_EMAIL_SCOPE])
+      	http = httplib2.Http()
+    	credentials.authorize(http)
+        return credentials.to_json()
+	    #if not http:
+	    #    http = httplib2.Http()
+	    #credentials.authorize(http)
+
+    	#temp hardcoded token
+        #token = 'ya29.CjjlAlrvqUwXrujCnJuqa08HTtmNilyP7K1GGrHQ40Gt489H6NGT9WQAxEL92OSQ6anGYeFPRcvI4g'
+
+        
+
+
+
+        tokenBearer = 'Bearer %s' % token
+        url = 'https://admin-dot-mq-vouchers.appspot.com/api/communities/mtv1/campaigns?page=0&size=1000&sorting=campaignName,ASC'
+        req = urllib2.Request(url, headers = {'Content-Type': 'application/json', 'Authorization' : tokenBearer})
+        f = urllib2.urlopen(req)
+        response = f.read()
+        sys.stdout.write(str(response))
+        respjson = json.loads(response)
+        
+        f.close()
+        #respjson = '3333'
+        #sys.stdout.write(str(all))
+        return str(response)
+    except urllib2.HTTPError, error:
+    	return ('get failed %s' % error)
+
+
+
     """Return a friendly HTTP greeting."""
-    return 'Hello World! from a Managed VM'
+    
 
 
 if __name__ == '__main__':
